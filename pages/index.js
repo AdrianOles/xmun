fd30d81fe978bd42c646ef91b4fc0974e573d248
AdrianOles <olesnieadrian@gmail.com>
2023-02-03 14:34:33 -0500

    Adjusted transition

--- pages/index.js
+++ pages/index.js
@@ -58,17 +58,17 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="border-t-2 border-b-2 border-purple-600 my-10 py-6 text-center mx-auto w-[350px] md:w-[500px] text-4xl
-      tracking-wide text-purple-900">
-        <motion.div
-            initial={{ opacity: 0 }}
-            whileInView={{ opacity: 1 }}
-            viewport={{ once: true }}
-            transition={{type: "easein", duration: .60, delay: .15}}
-          >
-          ST.FX WELCOMES
-          </motion.div>
-      </div>
+      <motion.div
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          viewport={{ once: true }}
+          transition={{type: "easein", duration: .60, delay: .15}}
+        >
+        <div className="border-t-2 border-b-2 border-purple-600 my-10 py-6 text-center mx-auto w-[350px] md:w-[500px] text-4xl
+        tracking-wide text-purple-900">
+            ST.FX WELCOMES
+        </div>
+      </motion.div>
 
       <motion.div
             initial={{ opacity: 0 }}
