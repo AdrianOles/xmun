6bff5d880775e48f6b308e9dbe82e49394654699
AdrianOles <olesnieadrian@gmail.com>
2023-02-04 17:26:09 -0500

    Rhea

--- pages/photos.js
+++ pages/photos.js
@@ -1,8 +1,35 @@
 import React from 'react'
+import Image from 'next/image'
+import rhea1 from '../public/rhea/rhea1.png'
+import rhea2 from '../public/rhea/rhea2.png'
+import rhea3 from '../public/rhea/rhea3.png'
+import rhea4 from '../public/rhea/rhea4.png'
+
 
 function photos() {
   return (
-    <div>photos</div>
+    <div className="flex flex-wrap mx-auto w-full px-auto gap-2">
+      <Image
+        src={rhea1}
+        height={500}
+        width={500}
+      />
+      <Image
+        src={rhea2}
+        height={500}
+        width={500}
+      />
+      <Image
+        src={rhea3}
+        height={500}
+        width={500}
+      />
+      <Image
+        src={rhea4}
+        height={500}
+        width={500}
+      />
+    </div>
   )
 }
 
--- pages/resources.js
+++ pages/resources.js
@@ -1,8 +1,36 @@
 import React from 'react'
+import Image from 'next/image'
+import rhea1 from '../public/rhea/rhea1.png'
+import rhea2 from '../public/rhea/rhea2.png'
+import rhea3 from '../public/rhea/rhea3.png'
+import rhea4 from '../public/rhea/rhea4.png'
 
 function resources() {
   return (
-    <div>resources</div>
+    <div>
+      <div className="flex flex-wrap mx-auto w-full px-auto gap-2">
+        <Image
+          src={rhea1}
+          height={500}
+          width={500}
+        />
+        <Image
+          src={rhea2}
+          height={500}
+          width={500}
+        />
+        <Image
+          src={rhea3}
+          height={500}
+          width={500}
+        />
+        <Image
+          src={rhea4}
+          height={500}
+          width={500}
+        />
+      </div>
+    </div>
   )
 }
 
