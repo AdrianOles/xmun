a42ca990bc3b991b0585e5c030a08ed5c34890fc
AdrianOles <olesnieadrian@gmail.com>
2023-02-04 14:32:22 -0500

    Added animation to itinerary page

--- pages/itinerary.js
+++ pages/itinerary.js
@@ -6,6 +6,7 @@ import purpleConference from '../public/purpleConference.png';
 import lunch from '../public/lunch.png';
 import breakIcon from '../public/breakIcon.png';
 import award from '../public/award.png';
+import { motion } from "framer-motion"
 
 function itinerary() {
 
@@ -42,8 +43,44 @@ function itinerary() {
     },
   ]
 
+  const variants = {
+    hidden: { opacity: 0 },
+    show: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.1,
+      },
+    },
+  };
+
+  const items = {
+    hidden: {
+      opacity: 0,
+      x: 100,
+    },
+    show : {
+      opacity: 1,
+      x:0,
+      transition: {
+        duration: .50
+      },
+    },
+  }
+
   return (
     <div className='w-full h-full'>
+      <div className='bg-gradient-to-tl to-purple-800 from-sky-700 h-[40vh] w-full relative top-0 shadow-xl'>
+        <img src="https://isp.hcdsb.org/wp-content/uploads/2018/07/JV5-700x400.jpg"
+          className='w-full h-full object-cover absolute mix-blend-overlay brightness-80' />
+        <div className='mx-auto w-full h-full px-10 flex flex-col gap-3 sm:px-20 md:px-36 lg:px-56'>
+          <div className='text-white my-auto mx-auto flex flex-col text-center'>
+            <div className='text-6xl font-bold mb-2'>Conference <span className="text-cyan-400">Itinerary</span></div>
+            <div>
+              Check it out to plan ahead and not miss a minute.
+            </div>
+          </div>
+        </div>
+      </div>
       {/* schedule */}
       <div className='flex h-full w-full lg:w-[60%] mx-auto'>
         {/* times/timeline */}
@@ -59,29 +96,34 @@ function itinerary() {
         {/* cards/events */}
         <div className=' w-[75%] border-l-2 border-gray-200 h-fit'>
           <div className='border-b-2 border-t-2 px-8 py-6 border-gray-200'>
-            <div className='text-[1.7em] font-semibold tracking-widest'>Itinerary</div>
+            <div className='text-[1.7em] font-semibold tracking-widest'>May 1st, 2023</div>
           </div>
+          
           <div className="flex flex-col h-full px-4 py-6">
-            {
-              events.map((event, index) => (
-                <div className={`border-b-2 border-gray-200 class-${index} mb-6`}>
-                  <div className={`items-center gap-4 p-4 pl-4 h-fit w-full bg-purple-500 schedule-bg-${index} ring-2 ring-purple-600 rounded-lg mb-6 flex`}>
-                    <div>
-                      <Image
-                        src={event.icon}
-                        alt="icon"
-                        width={30}
-                        height={30}
-                      />
+            <motion.div initial="hidden" animate="show" variants={variants}>
+              {
+                events.map((event, index) => (
+                  <div className={`border-b-2 border-gray-200 class-${index} mb-6`}>
+                    <motion.div variants={items}>
+                    <div className={`items-center gap-4 p-4 pl-4 h-fit w-full bg-purple-500 schedule-bg-${index} ring-2 ring-purple-600 rounded-lg mb-6 flex`}>
+                      <div>
+                        <Image
+                          src={event.icon}
+                          alt="icon"
+                          width={30}
+                          height={30}
+                        />
+                      </div>
+                      <div>
+                        <div>{event.name}</div>
+                        <div>{event.time}</div>
+                      </div>
                     </div>
-                    <div>
-                      <div>{event.name}</div>
-                      <div>{event.time}</div>
-                    </div>
+                    </motion.div>
                   </div>
-                </div>
-              ))
-            }
+                ))
+              }
+            </motion.div>
           </div>
         </div>
       </div>
