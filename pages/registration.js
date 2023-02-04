d76a0181dbf593b2344b72cfd3cabc6b367746f0
AdrianOles <olesnieadrian@gmail.com>
2023-02-04 17:07:49 -0500

    Updating responsiveness

--- pages/registration.js
+++ pages/registration.js
@@ -8,13 +8,13 @@ function registration() {
         <img src="https://isp.hcdsb.org/wp-content/uploads/2018/07/JV4-700x400.jpg"
           className='w-full h-full object-cover absolute mix-blend-overlay brightness-90' />
         <div className='mx-auto w-full py-[12em] px-6 flex flex-col gap-3 sm:px-20 md:px-36 lg:px-56 text-center'>
-          <div className='text-white text-5xl sm:text-6xl font-bold'>
+          <div className='text-white text-4xl sm:text-6xl font-bold'>
             <Link href="https://www.instagram.com/rhea.g.l/">REGISTRATION</Link>
           </div>
-          <div className="text-sky-100 text-2xl font-normal">
+          <div className="text-sky-100 text-xl sm:text-2xl font-normal">
             May 1st, 2023
           </div>
-          <div className="text-sky-100 text-2xl font-normal">
+          <div className="text-sky-100 text-xl sm:text-2xl font-normal">
             St. Francis Xavier Secondary School, Milton ON
           </div>
           <div className="register1 mt-5 mx-auto">
