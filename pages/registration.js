9a1d878da26ee2adcddfb7142fa52c029f1b7d91
AdrianOles <olesnieadrian@gmail.com>
2023-02-17 16:34:10 -0500

    updated registration button

--- pages/registration.js
+++ pages/registration.js
@@ -9,7 +9,7 @@ function registration() {
           className='w-full h-full object-cover absolute mix-blend-overlay brightness-90' />
         <div className='mx-auto w-full py-[12em] px-6 flex flex-col gap-3 sm:px-20 md:px-36 lg:px-56 text-center'>
           <div className='text-white text-4xl sm:text-6xl font-bold'>
-            <Link href="https://www.instagram.com/rhea.g.l/">REGISTRATION</Link>
+            <Link href="#">REGISTRATION</Link>
           </div>
           <div className="text-sky-100 text-xl sm:text-2xl font-normal">
             Deadline: May 1st, 2023
@@ -19,7 +19,7 @@ function registration() {
           </div>
           <div className="register1 mt-5 mx-auto">
             <div className="wrapper cursor-pointer">
-              <Link href="https://www.instagram.com/rhea.g.l/"><span className="z-20">REGISTER</span></Link>
+              <Link href="#"><span className="z-20">REGISTER</span></Link>
             </div>
           </div>
         </div>
