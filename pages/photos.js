cdbf885d20456f7241b1b4a0f965168b285d4a68
AdrianOles <olesnieadrian@gmail.com>
2023-02-25 23:46:27 -0500

    Testing

--- pages/photos.js
+++ pages/photos.js
@@ -1,21 +1,21 @@
 import React from 'react'
-import Image from 'next/image'
-import image1 from './../public/images/image1.jpg'
-import image2 from './../public/images/image2.jpg'
-import image3 from './../public/images/image3.jpg'
-import image4 from './../public/images/image4.jpg'
-import image5 from './../public/images/image5.jpg'
-import image6 from './../public/images/image6.jpg'
-import image7 from './../public/images/image7.jpg'
-import image8 from './../public/images/image8.jpg'
-import image9 from './../public/images/image9.jpg'
-import image10 from './../public/images/image10.jpg'
-import image11 from './../public/images/image11.jpg'
-import image12 from './../public/images/image12.jpg'
-import image13 from './../public/images/image13.jpg'
-import image15 from './../public/images/image15.jpg'
-import image16 from './../public/images/image16.jpg'
-import image17 from './../public/images/image17.jpg'
+// import Image from 'next/image'
+// import image1 from './../public/images/image1.jpg'
+// import image2 from './../public/images/image2.jpg'
+// import image3 from './../public/images/image3.jpg'
+// import image4 from './../public/images/image4.jpg'
+// import image5 from './../public/images/image5.jpg'
+// import image6 from './../public/images/image6.jpg'
+// import image7 from './../public/images/image7.jpg'
+// import image8 from './../public/images/image8.jpg'
+// import image9 from './../public/images/image9.jpg'
+// import image10 from './../public/images/image10.jpg'
+// import image11 from './../public/images/image11.jpg'
+// import image12 from './../public/images/image12.jpg'
+// import image13 from './../public/images/image13.jpg'
+// import image15 from './../public/images/image15.jpg'
+// import image16 from './../public/images/image16.jpg'
+// import image17 from './../public/images/image17.jpg'
 
 function photos() {
 
@@ -23,7 +23,7 @@ function photos() {
 
   return (
     <div className="flex flex-wrap w-full gap-4 p-4 items-center justify-center">
-      {
+      {/* {
         numbers.map((index) => (
           <div key={index} className="w-[70%] sm:w-[40%] md:w-[40%] h-[200px] md:h-[250px] lg:w-[20%] lg:h-[200px] relative">
               {
@@ -188,7 +188,7 @@ function photos() {
             }
           </div>
         ))
-      }
+      } */}
     </div>
   )
 }
