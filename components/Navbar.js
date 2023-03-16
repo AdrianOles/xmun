f346f5b1e4e9b968d8924f3d53d14edaadecfea1
AdrianOles <olesnieadrian@gmail.com>
2023-03-16 19:04:24 -0400

    Updated logo

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import Image from 'next/image'
 import { useRouter } from 'next/router';
+import logo from '../public/finally.png'
 
 function MobileNav({ open, setOpen }) {
     const router = useRouter();
@@ -32,27 +33,13 @@ export default function Navbar() {
             <MobileNav open={open} setOpen={setOpen} />
             <div className="w-full flex items-center justify-between z-50 header-blur sticky top-0  px-8 md:px-14 lg:px-24 shadow-xl">
                 <div className="py-4">
-                    <a className='flex items-center cursor-pointer' onClick={() => { router.push('/'); if(open) setOpen(!open)}}>
-                        <div className="relative">
-                            <div className="absolute -top-5 -left-[0.6257rem] scale-[60%]">
-                                <svg xmlnsXlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg" width="150" height="100">
-                                    <path style={{opacity:1, fill:'#0080ac', fillOpacity:'1', stroke:'#fff', strokeWidth:'2.16298', strokeDasharray:'none', strokeOpacity:'1'}} d="m49.238 5.974-.342.506.825 54.019 3.744.183 1.697-46.767z"/>
-                                    <path style={{opacity:1, fill:'#0080ac', fillOpacity:'1', stroke:'#fff', strokeWidth:'4.13695', strokeDasharray:'none', strokeOpacity:'1'}} d="m49.236 7.56-5.426 7.644 2.134 44.538 3.768.176.01.618 3.773.176 1.71-44.908-.023-.029.022-.589z"/>
-                                    <path style={{opacity:1, fill:'#00a1d9', fillOpacity:1, stroke:'#fff', strokeWidth:'2.06438', strokeDasharray:'none', strokeOpacity:1}} d="m46.319 62.904.035 8.7h-.002l.002.154v.225h.004l1.86 12.922c.887-.843.57-1.794 2.032-1.794 1.254 0 .421.364 1.303 1.01l1.872-12.396-.037-8.805Z"/>
-                                    <ellipse style={{opacity:1, fill:'#009dd3', fillOpacity:1, stroke:'#fff', strokeWidth:'1.54824', strokeDasharray:'none', strokeOpacity:1}} cx="49.738" cy="87.257" rx="3.724" ry="3.983"/>
-                                    <path style={{opacity:1, fill:'#0080ac', fillOpacity:1, stroke:'#fff', strokeWidth:'1.3424', strokeDasharray:'none', strokeOpacity:1}} d="m48.875 6.064-.414.523.997 55.766 4.527.19 2.05-48.28Z"/>
-                                    <path style={{opacity:1, fill:'#00a1d9', fillOpacity:1, stroke:'#fff', strokeWidth:0, strokeDasharray:'none', strokeOpacity:1}} d="m48.565 7.27-4.512 7.642 1.897 47.624 3.353.189-.731-54.806.027-.046Z"/>
-                                    <path style={{opacity:1, fill:'#fcfcfc', fillOpacity:1, stroke:'#fff', strokeWidth:'.350164', strokeDasharray:'none', strokeOpacity:1}} d="m38.064 59.72-4.544 3.095a.85.85 0 0 0 .211.026.884.884 0 0 0-.211.026l4.493 3.135 14.641.065v.002l.116-.002.166.002v-.002l10.024-.065 3.102-3.133a.406.406 0 0 0-.143-.026c.051 0 .098-.01.143-.028l-3.138-3.095-10.154.065z"/>
-                                    <path style={{opacity:1, fill:'#00a1d9', fillOpacity:1, stroke:'#fff',strokeWidth:'.31433', strokeDasharray:'none', strokeOpacity:1}} d="m38.42 60.1-4.395 2.58a.95.95 0 0 0 .204.021.982.982 0 0 0-.204.022l4.345 2.612 14.16.055v.002l.11-.002.161.002v-.002l9.694-.055 3-2.61a.45.45 0 0 0-.138-.022c.05 0 .095-.009.139-.023L62.46 60.1l-9.82.055z"/>
-                                </svg>
-                            </div>
-                            <Image
-                                src="https://1000logos.net/wp-content/uploads/2018/01/united-nations-logo.png"
-                                width={100}
-                                height={100}
-                                alt="Model UN Logo"
-                            />
-                        </div>
+                    <a className='flex items-center cursor-pointer gap-4' onClick={() => { router.push('/'); if(open) setOpen(!open)}}>
+                        <Image
+                            src={logo}
+                            width={75}
+                            height={75}
+                            alt="Model UN Logo"
+                        /> 
                         <div className='text-4xl font-bold text-sky-600'>
                             XMUN
                         </div>
