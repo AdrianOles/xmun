426e0edb2218737c2ce849ec08a8c0641bf42859
AdrianOles <olesnieadrian@gmail.com>
2023-04-05 11:38:21 -0400

    Changed font?

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -6,6 +6,10 @@ import {Navbar} from './'
 const Layout = ({children}) => {
     return (
       <>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+        <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet"/>
+        
         <div className='flex flex-col p-0 m-0'>
           {/* <Header /> */}
           <Navbar />
