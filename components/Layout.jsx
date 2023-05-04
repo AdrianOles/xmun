d8a09fa474f44d7c18fc206894e4db8d4595342d
AdrianOles <olesnieadrian@gmail.com>
2023-05-03 22:38:17 -0400

    Changed font

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -8,7 +8,7 @@ const Layout = ({children}) => {
       <>
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
-        <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet"/>
+        <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro&display=swap" rel="stylesheet"/>
         
         <div className='flex flex-col p-0 m-0'>
           <Navbar />
