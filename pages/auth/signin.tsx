4c24d52b51bf426b99348ca7a6c29e80e2c62807
AdrianOles <olesnieadrian@gmail.com>
2024-02-06 18:52:56 -0500

    please work

--- pages/auth/signin.tsx
+++ pages/auth/signin.tsx
@@ -16,6 +16,7 @@ export default function SignIn() {
                 // User is signed in
                 // You can redirect to a different page or perform other actions here
                 authUser.onUpdate(user.displayName, user.email, user.photoURL);
+                setLoading(false);
             } else {
                 // User is signed out
                 setLoading(false);
@@ -32,6 +33,17 @@ export default function SignIn() {
         setLoading(true);
         signInWithRedirect(auth, provider);
         setLoading(false);
+    }
+
+    const handleButton = () => {
+        if (!loading && !authUser.auth) {
+            signIn();
+        } else if (loading) {
+            return null;
+        } else if (authUser.auth) {
+            console.log("hi")
+            router.replace('/')
+        }
     }
 
     return (
@@ -65,15 +77,15 @@ export default function SignIn() {
                     <div className="text-[20px] lg:text-[26px] font-bold">Welcome Delegate,</div>
                     <div className="lg:text-base text-[14px]">Please sign in to continue your registration.</div>
                 </div>
-                <div className='md:text-[20px] text-[16px] text-white lg:w-[80%] w-full cursor-pointer relative flex items-center justify-center overflow-hidden transition mt-4 rounded-[8px] bg-black hover:bg-opacity-[80%]'>
-                    <div className="">
+                <div onClick={() => handleButton()} className='md:text-[20px] text-[16px] text-white lg:w-[80%] w-full cursor-pointer relative flex items-center justify-center overflow-hidden transition mt-4 rounded-[8px] bg-black hover:bg-opacity-[80%]'>
+                    <div className="z-50 px-3 py-2">
                         {
                             loading && !authUser ? (
-                                <span className="z-50 px-3 py-2">Loading...</span>
-                            ) : !authUser ?(
-                                <span onClick={() => signIn()} className="z-50 px-3 py-2">Sign In</span>
+                                <span>Loading...</span>
+                            ) : !loading && !authUser.auth ? (
+                                <span>Sign In</span>
                             ) : (
-                                <span className="z-50 px-3 py-2" onClick={() => router.push('/')}>Continue</span> 
+                                <span>Continue</span>  
                             )
                         }
                     </div>
